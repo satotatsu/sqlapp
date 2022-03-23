8d04d2b2d06f5a1d69ffb77ffcd1d814abdcd1ad
satot <satot@host.docker.internal>
2022-03-23 17:18:06 +0900

    fix

--- sqlapp-gradle-plugin/src/main/groovy/com/sqlapp/gradle/plugins/AbstractTask.groovy
+++ sqlapp-gradle-plugin/src/main/groovy/com/sqlapp/gradle/plugins/AbstractTask.groovy
@@ -29,9 +29,9 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Optional;
 import org.gradle.api.tasks.TaskAction;
+import org.gradle.api.tasks.Internal;
 
 import com.sqlapp.util.CommonUtils;
-import groovy.transform.Internal
 
 abstract class AbstractTask extends DefaultTask {
 	
