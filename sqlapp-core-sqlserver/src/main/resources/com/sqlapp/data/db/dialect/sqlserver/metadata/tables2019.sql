4bf8b3de34f7f73024eef57033c67a3f7963ca6a
satot <satot@host.docker.internal>
2022-03-28 14:28:20 +0900

    fix

--- sqlapp-core-sqlserver/src/main/resources/com/sqlapp/data/db/dialect/sqlserver/metadata/tables2019.sql
+++ sqlapp-core-sqlserver/src/main/resources/com/sqlapp/data/db/dialect/sqlserver/metadata/tables2019.sql
@@ -9,14 +9,14 @@ SELECT
 	, t.lock_escalation_desc AS lock_escalation
 	, text_in_row_limit
 	, p.data_compression_desc AS data_compression
-	, IDX.compression_delay
+	, idx.compression_delay
 	, large_value_types_out_of_row
 	, COALESCE(objectproperty(T.object_id, 'TableHasVarDecimalStorageFormat'),0) AS has_var_decimal
 	, DSIDX.name AS file_group_name
 	, COALESCE(lob.Name,'') AS lob_file_group_name
 	, COALESCE(filestr.Name,'') AS stream_file_group_name
-	, COALESCE(CTT.is_track_columns_updated_on,0) AS is_track_columns_updated_on
-	, COALESCE(CTT.object_id,0) AS has_change_tracking
+	, COALESCE(ctt.is_track_columns_updated_on,0) AS is_track_columns_updated_on
+	, COALESCE(ctt.object_id,0) AS has_change_tracking
 	, CAST(ex.value AS NVARCHAR(4000)) AS remark
 	, ps.name AS partition_scheme
 	, t.is_filetable
@@ -29,23 +29,23 @@ SELECT
 	, t.is_memory_optimized
 	, t.temporal_type_desc
 	, IDX.optimize_for_sequential_key
-FROM sys.tables T
+FROM sys.tables t
 INNER JOIN sys.schemas s
-  ON (s.schema_id = T.schema_id)
-LEFT OUTER JOIN sys.indexes IDX
-  ON (IDX.object_id = T.object_id and IDX.index_id < 2)
+  ON (s.schema_id = t.schema_id)
+LEFT OUTER JOIN sys.indexes idx
+  ON (idx.object_id = t.object_id and IDX.index_id < 2)
 LEFT OUTER JOIN sys.partitions p
-  ON (IDX.object_id = p.object_id
-  AND IDX.index_id=p.index_id
+  ON (idx.object_id = p.object_id
+  AND idx.index_id=p.index_id
   AND p.partition_number=1)
 LEFT OUTER JOIN sys.data_spaces AS DSIDX
-  ON (DSIDX.data_space_id = IDX.data_space_id)
+  ON (DSIDX.data_space_id = idx.data_space_id)
 LEFT OUTER JOIN sys.data_spaces AS lob
-  ON (lob.data_space_id = T.lob_data_space_id)
+  ON (lob.data_space_id = t.lob_data_space_id)
 LEFT OUTER JOIN sys.data_spaces AS filestr
-  ON (filestr.data_space_id = T.filestream_data_space_id)
-LEFT OUTER JOIN sys.change_tracking_tables CTT
-  ON (CTT.object_id = T.object_id)
+  ON (filestr.data_space_id = t.filestream_data_space_id)
+LEFT OUTER JOIN sys.change_tracking_tables ctt
+  ON (ctt.object_id = t.object_id)
 LEFT OUTER JOIN sys.extended_properties ex
   ON (t.object_id = ex.major_id
   AND ex.minor_id = 0)
