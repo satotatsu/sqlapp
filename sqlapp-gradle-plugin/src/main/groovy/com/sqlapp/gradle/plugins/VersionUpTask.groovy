d576accda6489d69937480697e03ac8583831b83
murashi-sn <nsa.toshi.shi@gmail.com>
2024-09-16 13:53:01 +0900

    fix: Properties without annotations
    https://docs.gradle.org/7.4.1/userguide/validation_problems.html#missing_annotation

--- sqlapp-gradle-plugin/src/main/groovy/com/sqlapp/gradle/plugins/VersionUpTask.groovy
+++ sqlapp-gradle-plugin/src/main/groovy/com/sqlapp/gradle/plugins/VersionUpTask.groovy
@@ -31,6 +31,7 @@ import java.util.List;
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.Optional;
 import org.gradle.api.tasks.TaskAction;
 
@@ -79,6 +80,7 @@ class VersionUpTask extends AbstractDbTask {
 		}
 	}
 
+	@Internal
 	protected VersionUpPojo getVersionUpPojo(){
 		return project.versionUp;
 	}
